70df5f9a044ccf952bf3b58022d3866b819f3864
default <dummy@ma.il>
2023-05-12 04:51:11 +0900

    wip:

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bytes"
+	_ "bytes"
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-playground/validator/v10"
+	cp "github.com/otiai10/copy"
 	"github.com/tidwall/gjson"
 	_ "github.com/tidwall/gjson"
 	"github.com/yuin/goldmark"
@@ -15,6 +16,7 @@ import (
 	_ "go.abhg.dev/goldmark/frontmatter"
 	"golang.org/x/exp/slices"
 	_ "golang.org/x/exp/slices"
+	"os"
 )
 
 const TES = `---
@@ -23,70 +25,69 @@ modification date: Thursday 30th June 2022 16:16:55
 # tags:
 # - publish
 # - jq
-# draft: false
+draft: true
 hi: greeting
 ---
 
 ## head2
 new news is here
 
-tag: publish
-draft: false
+end
 `
 
 /* `tag: publish` or `draft: false` */
-func publishFilter(meta string) bool {
-	//println(meta)
+func publishFilter(front string) bool {
 
-	draftGj := gjson.Get(meta, "draft")
-	tagsGj := gjson.Get(meta, "tags")
+	//println(front)
+	//println("---")
 
+	draftGj := gjson.Parse(front).Get("draft")
 	var isDraft bool
-	err := json.Unmarshal([]byte(draftGj.String()), &isDraft)
-	if err != nil {
-		isDraft = true
-		err = nil
-	}
-	//println("isDraft: ", isDraft)
 
+	err := json.Unmarshal([]byte(draftGj.String()), &isDraft)
+
+	if err != nil {
+		println("no draft")
+		isDraft = true
+	} else {
+		//draftGj.String()
+		println(" draft!!!")
+		//isDraft = true
+		isDraft = draftGj.Bool()
+	}
+
+	tagsGj := gjson.Parse(front).Get("tags")
 	var tags []string
 	var isTagContain bool
 	err = json.Unmarshal([]byte(tagsGj.String()), &tags)
 	if err != nil {
-		isTagContain = false
+		println("!!!!!tags not found!!!!!!!")
+		isTagContain = slices.Contains(tags, "publish")
 	} else {
+		println("????????????")
 		isTagContain = slices.Contains(tags, "publish")
 	}
 
 	for i, s := range tags {
-		fmt.Println("tag", i, s)
+		fmt.Println("tags:", i, s)
 	}
-	//err = slices.Contains(tags, "publish")
 
+	//isPublish := !isDraft
 	isPublish := !isDraft || isTagContain
+
+	//println("isDraft:", isDraft)
+	//println("isTagContain:", isTagContain)
+	//println("isPublish:", isPublish)
 	return isPublish
 }
 
-func main() {
-
-	//res, err := run(&req)
-	res, err := run(TES)
+func fileFilter(fileInfo string) (bool, error) {
+	//println(fileInfo)
+	bytes, err := os.ReadFile(fileInfo)
 	if err != nil {
-		res = &response{Error: err.Error()}
+		return false, fmt.Errorf("err open file: %w", err)
 	}
-	//_ = res
 
-	println(publishFilter(res.Frontmatter))
-
-}
-
-type response struct {
-	HTML        string
-	Frontmatter string
-	Error       string
-}
-
-func run(str string) (*response, error) {
 	var formats []frontmatter.Format
 
 	md := goldmark.New(
@@ -98,28 +99,50 @@ func run(str string) (*response, error) {
 	)
 
 	ctx := parser.NewContext()
-	var buf bytes.Buffer
-	if err := md.Convert([]byte(str), &buf, parser.WithContext(ctx)); err != nil {
-		return nil, fmt.Errorf("convert markdown: %w", err)
+	//var bb io.Writer
+	if err := md.Convert([]byte(bytes), os.Stdout, parser.WithContext(ctx)); err != nil {
+		//if err := md.Convert([]byte(bytes), os.Stdout); err != nil {
+		return false, fmt.Errorf("convert markdown: %w", err)
 	}
 
 	var fm string
 	if data := frontmatter.Get(ctx); data != nil {
 		var meta map[string]any
 		if err := data.Decode(&meta); err != nil {
-			return nil, fmt.Errorf("decode frontmatter: %w", err)
+			return false, fmt.Errorf("decode frontmatter: %w", err)
 		}
 
 		formatted, err := json.MarshalIndent(meta, "", "  ")
 		if err != nil {
-			return nil, fmt.Errorf("format frontmatter: %w", err)
+			return false, fmt.Errorf("format frontmatter: %w", err)
 		}
 
 		fm = string(formatted)
 	}
+	// true:skip, false:copy
+	return !publishFilter(fm), nil
+}
 
-	return &response{
-		HTML:        buf.String(),
-		Frontmatter: fm,
-	}, nil
+func main() {
+	////res, err := run(&req)
+	//res := publishFilter(TES)
+	//
+	//println(res)
+	////if err != nil {
+	////	res = &response{Error: err.Error()}
+	////}
+	////_ = res
+
+	err := cp.Copy(
+		"C:\\Users\\E14\\Downloads\\tempdir\\tempdir",
+		"C:\\Users\\E14\\Downloads\\tempdir\\piyo",
+		cp.Options{
+			Skip: func(info os.FileInfo, src, dest string) (bool, error) {
+				isSkip, err := fileFilter(src)
+				return isSkip, err
+			},
+		},
+	)
+	fmt.Println(err) // nil
+
 }
